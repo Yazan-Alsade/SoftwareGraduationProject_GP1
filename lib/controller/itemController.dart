bff6cf1997c120c066657168e781de700a734067
Yazan-Alsade <yazanalsade327@gmail.com>
2023-05-06 17:11:33 +0300

    material page

---
+++ lib/controller/itemController.dart
@@ -0,0 +1,10 @@
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+abstract class ItemsController extends GetxController {}
+
+class ItemControllerImp extends ItemsController {
+  @override
+  void onInit() {}
+
+  List categories = [];
+}
--- lib/special_pages/equipment.dart
+++ lib/special_pages/equipment.dart
@@ -2,6 +2,8 @@ import 'package:construction_company/special_pages/itemsEquipment.dart';
 import 'package:construction_company/widget/equipment/appBarEquip.dart';
 import 'package:flutter/material.dart';
 import 'package:scroll_snap_list/scroll_snap_list.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Equipment extends StatefulWidget {
   const Equipment({Key? key}) : super(key: key);
@@ -11,13 +13,52 @@ class Equipment extends StatefulWidget {
 }
 
 class _EquipmentState extends State<Equipment> {
-  List<String> images = [
-    "images/excavator (1).png",
-    "images/excavator (1).png",
-    "images/excavator (1).png",
-    "images/excavator (1).png",
-    "images/excavator (1).png",
-  ];
+//////////////// For all categories
+  Future<List<dynamic>> getCategories() async {
+    final response =
+        await http.get(Uri.parse('http://10.0.2.2:3000/Materials/Category'));
+    print(response.statusCode);
+
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body);
+      return decoded['categories'];
+    } else {
+      throw Exception('Failed to load categories');
+    }
+  }
+
+  // List<dynamic> materials = [];
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   fetchMaterials();
+  // }
+
+  //////////// For all materials
+  // Future<List<dynamic>> fetchMaterials() async {
+  //   try {
+  //     final response = await http
+  //         .get(Uri.parse('http://10.0.2.2:3000/Materials/GetAllMaterial'));
+  //     final data = jsonDecode(response.body);
+  //     setState(() {
+  //       return data['ShowMaterial'];
+  //     });
+  //   } catch (error) {
+  //     print('Error fetching materials: $error');
+  //   }
+  // }
+  Future<List<dynamic>> fetchMaterial() async {
+    final response = await http
+        .get(Uri.parse('http://10.0.2.2:3000/Materials/GetAllMaterial'));
+    print(response.statusCode);
+    if (response.statusCode == 201) {
+      final decoded = jsonDecode(response.body);
+      return decoded['ShowMaterial'];
+    } else {
+      throw Exception('Failed to load categories');
+    }
+  }
+
   List<String> items = [
     "images/istockphoto-1288941637-612x612.jpg",
     "images/istockphoto-1288941637-612x612.jpg",
@@ -25,8 +66,6 @@ class _EquipmentState extends State<Equipment> {
     "images/istockphoto-1288941637-612x612.jpg",
     "images/istockphoto-1288941637-612x612.jpg",
   ];
-
-  List categories = [];
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -87,203 +126,234 @@ class _EquipmentState extends State<Equipment> {
             height: 20,
           ),
           Text(
-            "Categories of Equipment",
+            "Categories of Equipment and Materials",
             style: TextStyle(
-                fontSize: 18,
-                color: Colors.teal[400],
-                fontWeight: FontWeight.bold),
+              fontSize: 18,
+              color: Colors.teal[400],
+            ),
           ),
           SizedBox(
             height: 15,
           ),
-          Container(
-            height: 120,
-            child: ListView.separated(
-              separatorBuilder: (context, index) => SizedBox(
-                width: 10,
-              ),
-              itemCount: images.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return Card(
-                  clipBehavior: Clip.antiAlias,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Image.asset(
-                        "images/istockphoto-1288941637-612x612.jpg",
-                        height: 240,
-                        fit: BoxFit.cover,
-                      ),
-                      Positioned(
-                        bottom: 8,
-                        right: 17,
-                        left: 30,
-                        child: Text(
-                          "Card with splash",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 20),
+          FutureBuilder(
+            future: getCategories(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              } else {
+                var categories = snapshot.data as List<dynamic>;
+                categories = categories.reversed.toList();
+
+                return Container(
+                  height: 140,
+                  child: ListView.separated(
+                    separatorBuilder: (context, index) => SizedBox(width: 10),
+                    itemCount: categories.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      final category = categories[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return ItemsEquipment();
+                          }));
+                        },
+                        child: Column(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Color(0xfff7b858),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              height: 100,
+                              child: Image.network(
+                                category['imageUrl'],
+                                height: 100,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              category['name'],
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
+                          ],
                         ),
-                      )
-                    ],
+                      );
+                    },
                   ),
                 );
-              },
+              }
+            },
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Material and Equipment for you:",
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.teal[400],
             ),
           ),
           SizedBox(
             height: 20,
           ),
-          Text(
-            "Equipment for you:",
-            style: TextStyle(
-                fontSize: 18,
-                color: Colors.teal[400],
-                fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 20,
-          ),
+          
           Container(
-              height: 200,
-              child: ScrollSnapList(
-                itemBuilder: _buildListItem,
-                itemCount: items.length,
-                itemSize: 150,
-                onItemFocus: (index) {},
-                dynamicItemSize: true,
-              )),
-          SizedBox(
-            height: 15,
-          ),
-          Text(
-            "Categories of Material",
-            style: TextStyle(
-                fontSize: 18,
-                color: Colors.teal[400],
-                fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Container(
-            height: 150,
-            child: ListView.separated(
-              separatorBuilder: (context, index) => SizedBox(
-                width: 10,
-              ),
-              itemCount: images.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return Card(
-                  clipBehavior: Clip.antiAlias,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Image.asset(
-                        "images/istockphoto-1288941637-612x612.jpg",
-                        height: 240,
-                        fit: BoxFit.cover,
-                      ),
-                      Positioned(
-                        bottom: 8,
-                        right: 17,
-                        left: 30,
-                        child: Text(
-                          "Card with splash",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 20),
+            height: 200,
+            // width: 200,
+            child: FutureBuilder(
+              future: getCategories(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                } else {
+                  var materials = snapshot.data as List<dynamic>;
+                  materials = materials.reversed.toList();
+
+                  return ScrollSnapList(
+                    dynamicItemSize: true,
+                    itemSize: 150,
+                    itemBuilder: (context, index) {
+                      var material = materials[index];
+
+                      return Card(
+                        elevation: 12,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          child: Column(
+                            children: [
+                              Image.network(
+                                material['imageUrl'],
+                                fit: BoxFit.cover,
+                                height: 100,
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                material["name"],
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    // Text(
+                                    //   "\$${material['price']}",
+                                    //   style: TextStyle(
+                                    //       fontWeight: FontWeight.bold),
+                                    // ),
+                                    // Text(
+                                    //   material['unit'],
+                                    //   style: TextStyle(color: Colors.blue),
+                                    // ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                      )
-                    ],
-                  ),
-                );
+                      );
+                    },
+                    itemCount: materials.length,
+                    onItemFocus: (index) {
+                      // do something when item is focused
+                    },
+                  );
+                }
               },
             ),
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Material for you:",
-            style: TextStyle(
-                fontSize: 18,
-                color: Colors.teal[400],
-                fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-              height: 200,
-              child: ScrollSnapList(
-                itemBuilder: _buildListItem,
-                itemCount: items.length,
-                itemSize: 150,
-                onItemFocus: (index) {},
-                dynamicItemSize: true,
-              )),
-          SizedBox(
-            height: 20,
-          )
         ],
       ),
     );
   }
 
-  Widget _buildListItem(BuildContext context, int index) {
-    return SizedBox(
-      width: 150,
-      height: 300,
-      child: Card(
-        elevation: 12,
-        child: ClipRRect(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          child: Column(
-            children: [
-              Image.asset(
-                items[index],
-                fit: BoxFit.cover,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Excavtor",
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "1900",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "Review",
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  // Widget _buildListItem(BuildContext context, int index) {
+  //    FutureBuilder(
+  //     future: fetchMaterial(),
+  //     builder: (context, snapshot) {
+  //       if (snapshot.connectionState == ConnectionState.waiting) {
+  //         return Center(
+  //           child: CircularProgressIndicator(),
+  //         );
+  //       } else if (snapshot.hasError) {
+  //         return Text('${snapshot.error}');
+  //       } else {
+  //         var materials = snapshot.data as List<dynamic>;
+  //         materials = materials.reversed.toList();
+
+  //         return ScrollSnapList(
+  //           itemSize: 300,
+  //           itemBuilder: (context, index) {
+  //             var material = materials[index];
+
+  //             return Card(
+  //               elevation: 12,
+  //               child: ClipRRect(
+  //                 borderRadius: BorderRadius.all(Radius.circular(10)),
+  //                 child: Column(
+  //                   children: [
+  //                     Image.network(
+  //                       material['media'],
+  //                       fit: BoxFit.cover,
+  //                       height: 200,
+  //                     ),
+  //                     SizedBox(
+  //                       height: 10,
+  //                     ),
+  //                     Text(
+  //                       material["name"],
+  //                       style: TextStyle(
+  //                         fontSize: 15,
+  //                       ),
+  //                     ),
+  //                     Padding(
+  //                       padding: const EdgeInsets.symmetric(horizontal: 8.0),
+  //                       child: Row(
+  //                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //                         children: [
+  //                           Text(
+  //                             "\$${material['price']}",
+  //                             style: TextStyle(fontWeight: FontWeight.bold),
+  //                           ),
+  //                           Text(
+  //                             material['unit'],
+  //                             style: TextStyle(color: Colors.blue),
+  //                           ),
+  //                         ],
+  //                       ),
+  //                     )
+  //                   ],
+  //                 ),
+  //               ),
+  //             );
+  //           },
+  //           itemCount: materials.length,
+  //           onItemFocus: (int index) {
+  //             // do something when item is focused
+  //           },
+  //         );
+  //       }
+  //     },
+  //   );
+  // }
 }
